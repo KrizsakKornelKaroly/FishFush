20e6379c8ff3b073fffe56f59bbc6dd24e25ca15
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2025-11-20 14:07:34 +0100

    UI design version 1

--- FishFush/Program.cs
+++ FishFush/Program.cs
@@ -10,6 +10,25 @@ namespace FishFush
     {
         static void Main(string[] args)
         {
+            UI();
+        }
+
+
+
+
+        public static void UI()
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("\t\t+----- FishFush - Halnyilvántartó -----+");
+            Console.WriteLine("\t\t|                                      |");
+            Console.WriteLine("\t\t|          1. Hal hozzáadása           |");
+            Console.WriteLine("\t\t|          2. Halak listázása          |");
+            Console.WriteLine("\t\t|          3. Hal módosítása           |");
+            Console.WriteLine("\t\t|             4. Kilépés               |");
+            Console.WriteLine("\t\t|                                      |");
+            Console.WriteLine("\t\t+--------------------------------------+");
         }
     }
 }
